501c38d50bf829f89921f4216e59e9ed75c85ac1
Alan <alandecker94@yahoo.com>
2020-09-20 00:16:28 -0500

    One last adjustment before release 2
    ~up menu sound plays when animation occurs

--- src/mainui/basemenu.cpp
+++ src/mainui/basemenu.cpp
@@ -958,6 +958,8 @@ void UI_PushMenu( menuFramework_s *menu )
 	int		i;
 	menuCommon_s	*item;
 
+	UI_StartSound(uiSoundIn); //magic nipples - put upmenu sound here to match downmenu
+
 	// if this menu is already present, drop back to that level to avoid stacking menus by hotkeys
 	for( i = 0; i < uiStatic.menuDepth; i++ )
 	{
@@ -1102,11 +1104,11 @@ void UI_UpdateMenu( float flTime )
 
 	// delay playing the enter sound until after the menu has been
 	// drawn, to avoid delay while caching images
-	if( uiStatic.enterSound > 0.0f && uiStatic.enterSound <= gpGlobals->time )
+	/*if( uiStatic.enterSound > 0.0f && uiStatic.enterSound <= gpGlobals->time )
 	{
 		UI_StartSound( uiSoundIn );
 		uiStatic.enterSound = -1;
-	}
+	}*/
 }
 
 /*
