03df03e2770c1592fc9898cc724428e0ff19598e
Alan <alandecker94@yahoo.com>
2022-08-16 17:41:28 -0500

    Completely reworked view scaling code. Proper square pixels now at any aspect ratio, and unneeded junk code removed.

--- src/engine/client/gl_downsample.c
+++ src/engine/client/gl_downsample.c
@@ -1,7 +1,7 @@
 /*
 gl_downsample.c - take a shot of the current resolution and shrink it by a factor
 
-Copyright (C) 2019 Magic_Nipples
+Copyright (C) 2022 Magic_Nipples
 */
 
 #include "common.h"
@@ -13,7 +13,6 @@ static int DOWNSAMPLE_SIZE_Y;
 
 static int r_initsampletexture;
 static int r_sampleeffecttexture;
-static int r_samplebackuptexture;
 static int r_downsampletexture;
 
 static int r_screendownsamplingtexture_size;
@@ -37,17 +36,12 @@ static int sample_height;
 static float sampleText_tcw;
 static float sampleText_tch;
 
-/*
-=================
-R_Bloom_InitBackUpTexture
-=================
-*/
-static void R_Bloom_InitBackUpTexture(int width, int height)
+int TexMgr_Pad(int s)
 {
-	r_screenbackuptexture_width = width;
-	r_screenbackuptexture_height = height;
-
-	r_samplebackuptexture = GL_CreateTexture("*samplebackuptexture", width, height, NULL, TF_NEAREST);
+	int i;
+	for (i = 1; i < s; i <<= 1)
+		;
+	return i;
 }
 
 /*
@@ -60,19 +54,52 @@ static void R_Bloom_InitEffectTexture(void)
 	if (r_downsample->value < 0)
 		r_downsample->value = 0;
 
-	if (r_downsample->value > 5.0f)
-		r_downsample->value = 5.0f;
+	if (r_downsample->value > 4.0f)
+		r_downsample->value = 4.0f;
+
+	r_downsample->value = (int)r_downsample->value;
 
 	/*if (cl.local.waterlevel >= 3)
 	{
 		DOWNSAMPLE_SIZE_X = screen_texture_width / (2.5);
 		DOWNSAMPLE_SIZE_Y = screen_texture_width / (2.5);
-	}
-	else*/
+	}*/
+
+	if (r_downsample->value == 1)
 	{
-		DOWNSAMPLE_SIZE_X = screen_texture_width / (r_downsample->value * 2);
-		DOWNSAMPLE_SIZE_Y = screen_texture_height / (r_downsample->value * 2);
+		if (glState.height > 480)
+			DOWNSAMPLE_SIZE_X = 480;
+		else
+			DOWNSAMPLE_SIZE_X = 320;
 	}
+	else if (r_downsample->value == 2)
+	{
+		if (glState.height > 480)
+			DOWNSAMPLE_SIZE_X = 320;
+		else
+			DOWNSAMPLE_SIZE_X = 240;
+	}
+	else if (r_downsample->value == 3)
+	{
+		if (glState.height > 480)
+			DOWNSAMPLE_SIZE_X = 240;
+		else
+			DOWNSAMPLE_SIZE_X = 160;
+	}
+	else if (r_downsample->value == 4)
+	{
+		if (glState.height > 480)
+			DOWNSAMPLE_SIZE_X = 160;
+		else
+			DOWNSAMPLE_SIZE_X = 80;
+	}
+	DOWNSAMPLE_SIZE_Y = DOWNSAMPLE_SIZE_X;
+
+	/*if (!GL_Support(GL_ARB_TEXTURE_NPOT_EXT))
+	{
+		DOWNSAMPLE_SIZE_X = TexMgr_Pad(DOWNSAMPLE_SIZE_X);
+		DOWNSAMPLE_SIZE_Y = TexMgr_Pad(DOWNSAMPLE_SIZE_Y);
+	}*/
 
 	r_sampleeffecttexture = GL_CreateTexture("*sampleeffecttexture", DOWNSAMPLE_SIZE_X, DOWNSAMPLE_SIZE_Y, NULL, TF_NEAREST);
 }
@@ -87,11 +114,10 @@ static void R_Sampling_InitTextures(void)
 	screen_texture_width = glState.width;
 	screen_texture_height = glState.height;
 
-	// disable blooms if we can't handle a texture of that size
+	// disable downsample if we can't handle a texture of that size
 	if (screen_texture_width > glConfig.max_2d_texture_size || screen_texture_height > glConfig.max_2d_texture_size)
 	{
 		screen_texture_width = screen_texture_height = 0;
-		//ALERT( at_warning, "'R_InitBloomScreenTexture' too high resolution for light bloom, effect disabled\n" );
 		Con_Printf("'R_Sampling_InitTextures' too high of a resolution, effect disabled\n");
 		return;
 	}
@@ -104,8 +130,6 @@ static void R_Sampling_InitTextures(void)
 	// if screensize is more than 2x the bloom effect texture, set up for stepped downsampling
 	r_downsampletexture = 0;
 	r_screendownsamplingtexture_size = 0;
-
-	R_Bloom_InitBackUpTexture(DOWNSAMPLE_SIZE_X, DOWNSAMPLE_SIZE_Y);
 }
 
 /*
@@ -122,13 +146,10 @@ void R_InitDownSampleTextures(void)
 		GL_FreeTexture(r_initsampletexture);
 	if (r_sampleeffecttexture)
 		GL_FreeTexture(r_sampleeffecttexture);
-	if (r_samplebackuptexture)
-		GL_FreeTexture(r_samplebackuptexture);
 	if (r_downsampletexture)
 		GL_FreeTexture(r_downsampletexture);
 
-	r_initsampletexture = r_sampleeffecttexture = 0;
-	r_samplebackuptexture = r_downsampletexture = 0;
+	r_initsampletexture = r_sampleeffecttexture = r_downsampletexture = 0;
 
 	//if ((!r_downsample->value) && (cl.local.waterlevel < 3))
 	if (!r_downsample->value)
@@ -154,87 +175,6 @@ static _inline void R_Sampling_Quad(int x, int y, int w, int h, float texwidth, 
 	pglTexCoord2f(texwidth, texheight);
 	pglVertex2f(x + w, y);
 	pglEnd();
-}
-
-/*
-=================
-R_Sampling_DrawEffect
-=================
-*/
-static void R_Sampling_DrawEffect(void)
-{
-	GL_Bind(GL_TEXTURE0, r_sampleeffecttexture);
-
-	pglBegin(GL_QUADS);
-	pglTexCoord2f(0, sampleText_tch);
-	pglVertex2f(curView_x, curView_y);
-	pglTexCoord2f(0, 0);
-	pglVertex2f(curView_x, curView_y + curView_height);
-	pglTexCoord2f(sampleText_tcw, 0);
-	pglVertex2f(curView_x + curView_width, curView_y + curView_height);
-	pglTexCoord2f(sampleText_tcw, sampleText_tch);
-	pglVertex2f(curView_x + curView_width, curView_y);
-	pglEnd();
-}
-
-/*
-=================
-R_Sampling_GenerateTexture
-=================
-*/
-static void R_Sampling_GenerateTexture(void)
-{
-	// set up sample size workspace
-	pglScissor(0, 0, sample_width, sample_height);
-	pglViewport(0, 0, sample_width, sample_height);
-	pglMatrixMode(GL_PROJECTION);
-	pglLoadIdentity();
-	pglOrtho(0, sample_width, sample_height, 0, -10, 100);
-	pglMatrixMode(GL_MODELVIEW);
-	pglLoadIdentity();
-
-	// copy small scene into r_sampleeffecttexture
-	GL_Bind(GL_TEXTURE0, r_sampleeffecttexture);
-	pglCopyTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, 0, 0, sample_width, sample_height);
-
-	// restore full screen workspace
-	pglScissor(0, 0, glState.width, glState.height);
-	pglViewport(0, 0, glState.width, glState.height);
-	pglMatrixMode(GL_PROJECTION);
-	pglLoadIdentity();
-	pglOrtho(0, glState.width, glState.height, 0, -10, 100);
-	pglMatrixMode(GL_MODELVIEW);
-	pglLoadIdentity();
-}
-
-/*
-=================
-R_Downsample_View
-=================
-*/
-static void R_Downsample_View(void)
-{
-	// stepped downsample
-	int midsample_width = (r_screendownsamplingtexture_size * sampleText_tcw);
-	int midsample_height = (r_screendownsamplingtexture_size * sampleText_tch);
-
-	pglDisable(GL_BLEND);
-
-	// copy the screen and draw resized
-	GL_Bind(GL_TEXTURE0, r_initsampletexture);
-	pglCopyTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, curView_x, glState.height - (curView_y + curView_height), curView_width, curView_height);
-	R_Sampling_Quad(0, glState.height - midsample_height, midsample_width, midsample_height, screenTex_tcw, screenTex_tch);
-
-	// now copy into downsampling (mid-sized) texture
-	GL_Bind(GL_TEXTURE0, r_downsampletexture);
-	pglCopyTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, 0, 0, midsample_width, midsample_height);
-
-	// now draw again in bloom size
-	R_Sampling_Quad(0, glState.height - sample_height, sample_width, sample_height, sampleText_tcw, sampleText_tch);
-
-	// now blend the big screen texture into the bloom generation space (hoping it adds some blur)
-	GL_Bind(GL_TEXTURE0, r_initsampletexture);
-	R_Sampling_Quad(0, glState.height - sample_height, sample_width, sample_height, screenTex_tcw, screenTex_tch);
 }
 
 /*
@@ -295,30 +235,25 @@ void R_DownSampling(void)
 	sample_width = (DOWNSAMPLE_SIZE_X * sampleText_tcw);
 	sample_height = (DOWNSAMPLE_SIZE_Y * sampleText_tch);
 
-	// copy the screen space we'll use to work into the backup texture
-	GL_Bind(GL_TEXTURE0, r_samplebackuptexture);
-	pglCopyTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, 0, 0, r_screenbackuptexture_width * sampleText_tcw, r_screenbackuptexture_height * sampleText_tch);
+	// copy the screen and draw resized
+	GL_Bind(GL_TEXTURE0, r_initsampletexture);
+	pglCopyTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, curView_x, glState.height - (curView_y + curView_height), curView_width, curView_height);
+	R_Sampling_Quad(0, glState.height - sample_height, sample_width, sample_height, screenTex_tcw, screenTex_tch);
 
-	// create the bloom image
-	R_Downsample_View();
-	R_Sampling_GenerateTexture();
+	// copy small scene into r_sampleeffecttexture
+	GL_Bind(GL_TEXTURE0, r_sampleeffecttexture);
+	pglCopyTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, 0, 0, sample_width, sample_height);
 
-	pglDisable(GL_BLEND);
-	// restore the screen-backup to the screen
-	GL_Bind(GL_TEXTURE0, r_samplebackuptexture);
-
-
-	pglScissor(RI.viewport[0], RI.viewport[1], RI.viewport[2], RI.viewport[3]);
-
-	R_Sampling_DrawEffect();
-
-	pglViewport(RI.viewport[0], RI.viewport[1], RI.viewport[2], RI.viewport[3]);
-
-	pglMatrixMode(GL_PROJECTION);
-	GL_LoadMatrix(RI.projectionMatrix);
-
-	pglMatrixMode(GL_MODELVIEW);
-	GL_LoadMatrix(RI.worldviewMatrix);
+	pglBegin(GL_QUADS);
+	pglTexCoord2f(0, sampleText_tch);
+	pglVertex2f(curView_x, curView_y);
+	pglTexCoord2f(0, 0);
+	pglVertex2f(curView_x, curView_y + curView_height);
+	pglTexCoord2f(sampleText_tcw, 0);
+	pglVertex2f(curView_x + curView_width, curView_y + curView_height);
+	pglTexCoord2f(sampleText_tcw, sampleText_tch);
+	pglVertex2f(curView_x + curView_width, curView_y);
+	pglEnd();
 
 	pglEnable(GL_DEPTH_TEST);
 	pglDepthMask(GL_TRUE);
